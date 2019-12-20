73b66d59ac0730b87137a28bcc664fd034277219
bossikill <550476877@qq.com>
2019-12-20 18:07:33 +0800

    omitted

--- src/lambda/index5.java
+++ src/lambda/index5.java
@@ -2,14 +2,31 @@ package lambda;
 
 //访问变量
 public class index5 {
-    public static void main(String[] args) {
+    //实例成员变量
+    private int value = 10;
+    //静态成员变量
+    private static int staticValue = 5;
 
+    //静态方法,进行加密算法
+    public static Calculable1 add() {
+        Calculable1 result = (int a, int b) -> {
+            //访问静态成员变量,不能访问实例成员变量
+            staticValue++;
+            int c = a + b + staticValue;//this.value;
+            return c;
+        };
+        return result;
     }
 
-    /**
-     * 打印计算结果
-     */
-    public static void display(Calculable1 calc, int n1, int n2) {
-        System.out.println(calc.calculateInt(n1, n2));
+    //实例方法,进行减法运算
+    public Calculable1 sub() {
+        Calculable1 result = (int a, int b) -> {
+            //访问静态成员变量和实例成员变量
+            staticValue++;
+            this.value++;
+            int c = a - b - staticValue - this.value;
+            return c;
+        };
+        return result;
     }
 }
