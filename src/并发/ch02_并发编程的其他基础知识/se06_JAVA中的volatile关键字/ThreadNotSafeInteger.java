1303e1a1eb3df4c4bc913fa4c47d1dca07b1a824
bossikill <550476877@qq.com>
2020-01-01 06:43:27 +0800

    omitted

---
+++ src/并发/ch02_并发编程的其他基础知识/se06_JAVA中的volatile关键字/ThreadNotSafeInteger.java
@@ -0,0 +1,14 @@
+package 并发.ch02_并发编程的其他基础知识.se06_JAVA中的volatile关键字;
+
+//线程不安全
+public class ThreadNotSafeInteger {
+    private int value;
+
+    public int get() {
+        return value;
+    }
+
+    public void set(int value) {
+        this.value = value;
+    }
+}
---
+++ src/并发/ch02_并发编程的其他基础知识/se06_JAVA中的volatile关键字/ThreadSafeInteger.java
@@ -0,0 +1,14 @@
+package 并发.ch02_并发编程的其他基础知识.se06_JAVA中的volatile关键字;
+
+//使用synchronized进行同步
+public class ThreadSafeInteger {
+    private int value;
+
+    public synchronized int get() {
+        return value;
+    }
+
+    public synchronized void set(int value) {
+        this.value = value;
+    }
+}
