effb6e8123b8fa514396492f9930e6242d56df0f
bossikill <550476877@qq.com>
2020-01-20 10:44:16 +0800

    omitted

--- src/learn/HelloWorld.java
+++ src/learn/HelloWorld.java
@@ -9,18 +9,7 @@ import java.util.stream.Collectors;
 
 public class HelloWorld {
 
-    static final int SHARED_SHIFT = 16;
-    static final int SHARED_UNIT = (1 << SHARED_SHIFT);
-    static final int MAX_COUNT = (1 << SHARED_SHIFT) - 1;
-    static final int EXCLUSIVE_MASK = (1 << SHARED_SHIFT) - 1;
-
     public static void main(String[] args) {
-        int COUNT_BITS = Integer.SIZE - 3;
-        int CAPACITY = (1 << COUNT_BITS) - 1;
-        int RUNNING = -1 << COUNT_BITS;
-        int SHUTDOWN = 0 << COUNT_BITS;
-        int STOP = 1 << COUNT_BITS;
-        int TIDYING = 2 << COUNT_BITS;
-        int TERMINATED = 3 << COUNT_BITS;
+        System.out.println(123);
     }
 }
